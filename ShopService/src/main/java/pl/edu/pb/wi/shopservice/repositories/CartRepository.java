c10b4cf7f4414b0ac45a4cd86f723438c8c48506
pzajaczkowski <demoon.pps@gmail.com>
2025-05-27 20:19:19 +0200

    Refactor cart code by removing unused imports

--- ShopService/src/main/java/pl/edu/pb/wi/shopservice/repositories/CartRepository.java
+++ ShopService/src/main/java/pl/edu/pb/wi/shopservice/repositories/CartRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.edu.pb.wi.shopservice.entities.CartProduct;
 import pl.edu.pb.wi.shopservice.entities.CartProductId;
-import pl.edu.pb.wi.shopservice.entities.Product;
 
 @Repository
 public interface CartRepository extends JpaRepository<CartProduct, CartProductId> {
