6f16b434f5301c0754f0e9f00fb0173d58e94b67
Filip Drażba <fdrazba@gmail.com>
2025-06-26 22:47:05 +0200

    Added ApiGatewayService

---
+++ ApiGateway/src/test/java/pl/edu/pb/wi/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.wi.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
